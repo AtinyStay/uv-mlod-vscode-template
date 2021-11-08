d8eadcd747df1fa0ba349935cee68b07312c84a6
AtinyStay <72931462+AtinyStay@users.noreply.github.com>
2021-11-08 00:44:54 +0000

    manque supp

--- workspace/TP5/arbre-bin-recherche-main.c
+++ workspace/TP5/arbre-bin-recherche-main.c
@@ -5,7 +5,7 @@
 #include "arbre-bin-recherche.h"
 
 int main(void){
-	ArbreBinaire a; //,p;
+	ArbreBinaire a,p;
 
 	initialiser(&a);
 	// a = NULL;
@@ -36,10 +36,6 @@ int main(void){
 	afficheGRD_r(a);printf("\n");
 	printf("nombreDeNoeud(a)= %i\n",nombreDeNoeud(a));
 
-	a = pere(a,2);
-	afficheGRD_r(a);printf("\n");
-
-/*
 // 	a = supprimer_r(a,6);
 // 	afficheGRD_r(a);printf("\n");
 // 	printf("nombreDeNoeud(a)= %i\n",nombreDeNoeud(a));
@@ -48,33 +44,33 @@ int main(void){
 // 	afficheGRD_r(a);printf("\n");
 // 	printf("nombreDeNoeud(a)= %i\n",nombreDeNoeud(a));
 //
-	// printf("profondeur 5 = %i\n",profondeur(a,5));
-	// printf("profondeur 2 = %i\n",profondeur(a,2));
-	// printf("profondeur 3 = %i\n",profondeur(a,3));
-	// printf("profondeur 7 = %i\n",profondeur(a,7));
-	// printf("profondeur NULL = %i\n",profondeur(NULL,5));
-	// printf("profondeur 18 = %i\n",profondeur(a,18));
+	printf("profondeur 5 = %i\n",profondeur(a,5));
+	printf("profondeur 2 = %i\n",profondeur(a,2));
+	printf("profondeur 3 = %i\n",profondeur(a,3));
+	printf("profondeur 7 = %i\n",profondeur(a,7));
+	printf("profondeur NULL = %i\n",profondeur(NULL,5));
+	printf("profondeur 18 = %i\n",profondeur(a,18));
+
+	initialiser(&p);
+	p=pere(NULL,5);
+	printf("pere(NULL,5) = %i\n",p==NULL?-1:p->val);
+	p=pere(a,18);
+	printf("pere(18) = %i\n",p==NULL?-1:p->val);
+	p=pere(a,5);
+	printf("pere(5) = %i\n",p==NULL?-1:p->val);
+	p=pere(a,8);
+	printf("pere(8) = %i\n",p==NULL?-1:p->val);
+	p=pere(a,3);
+	printf("pere(3) = %i\n",p==NULL?-1:p->val);
 	//
-	//
-	// p=pere(NULL,5);
-	// printf("pere(NULL,5) = %i\n",p==NULL?-1:p->val);
-	// p=pere(a,18);
-	// printf("pere(18) = %i\n",p==NULL?-1:p->val);
-	// p=pere(a,5);
-	// printf("pere(5) = %i\n",p==NULL?-1:p->val);
-	// p=pere(a,8);
-	// printf("pere(8) = %i\n",p==NULL?-1:p->val);
-	// p=pere(a,3);
-	// printf("pere(3) = %i\n",p==NULL?-1:p->val);
-	//
-	// p=recherche_r(a, 18);
-	// printf("recherche(18) = %i\n",p==NULL?-1:p->val);
-	// p=recherche_r(NULL, 3);
-	// printf("recherche(NULL) = %i\n",p==NULL?-1:p->val);
-	// p=recherche_r(a, 3);
-	// printf("recherche(3) = %i\n",p==NULL?-1:p->val);
-	// p=recherche_r(a, 8);
-	// printf("recherche(8) = %i\n",p==NULL?-1:p->val);
+	p=recherche_r(a, 18);
+	printf("recherche(18) = %i\n",p==NULL?-1:p->val);
+	p=recherche_r(NULL, 3);
+	printf("recherche(NULL) = %i\n",p==NULL?-1:p->val);
+	p=recherche_r(a, 3);
+	printf("recherche(3) = %i\n",p==NULL?-1:p->val);
+	p=recherche_r(a, 8);
+	printf("recherche(8) = %i\n",p==NULL?-1:p->val);
 	//
 	// a = supprimer_r(a,18);
 	// printf("nombreDeNoeud(a)= %i\n",nombreDeNoeud(a));
@@ -87,7 +83,7 @@ int main(void){
 	// printf("suppression(5) : \n");afficheGDR_r(a);printf("\n");
 	// printf("nombreDeNoeud(a)= %i\n",nombreDeNoeud(a));
 
-	detruire_r(a);
-*/
+	//detruire_r(a);
+
 	return EXIT_SUCCESS;
 }
